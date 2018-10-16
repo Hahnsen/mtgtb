1394c52cd325617bc4cef5006ae207842c8d9559
Roland Hahn <hahnsen111@gmail.com>
2018-10-16 12:37:17 +0200

    rudimentary database

--- mtgtb/tradingbase/admin.py
+++ mtgtb/tradingbase/admin.py
@@ -2,5 +2,54 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django import forms
+from models import CardType, Color, Card
+import re
 
 # Register your models here.
+
+
+class CardTypeAdmin(admin.ModelAdmin):
+    """Admin for CardType."""
+    fields = ['name', 'speed']
+    list_display = ['name', 'speed']
+
+
+
+class CardForm(forms.ModelForm):
+    """Form for the cards."""
+    class Meta:
+        """Model to use."""
+        model = Card
+        exclude = ()
+        widgets = {
+            'colors': forms.CheckboxSelectMultiple()}
+
+    def _check_manacost(self, data):
+        """Check if manacost is entered correctly."""
+        manacost = data.get("manacost")
+        valid = re.compile('^[0-9]*(W|U|R|B|G)*$')
+        if valid.match(manacost) is None:
+            raise ValidationError("Please enter manacost in correct format, " +
+                                  "e.g. '3WURR' (number for mana of any color "
+                                  "+ capital color letters expressing the " +
+                                  "mana symbols on the card).")
+
+    def clean(self):
+        """Verify."""
+        cleaned_data = super(CardForm, self).clean()
+        self._check_manacost(cleaned_data)
+        return cleaned_data
+
+    
+class CardAdmin(admin.ModelAdmin):
+    """Admin for Card."""
+    form = CardForm
+    search_fields = ['name']
+    list_display = ['name', 'cardtype', 'manacost']
+
+
+admin.site.register(Color)
+admin.site.register(CardType, CardTypeAdmin)
+admin.site.register(Card, CardAdmin)
--- mtgtb/tradingbase/models.py
+++ mtgtb/tradingbase/models.py
@@ -4,3 +4,57 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class CardType(models.Model):
+    """The card type of each card."""
+
+    SPEED_CHOICES = (
+        ('S', 'sorcery'),
+        ('I', 'instant'))
+
+    name = models.CharField(max_length=20, null=False, unique=True)
+    speed = models.CharField(max_length=1,
+                             choices=SPEED_CHOICES)
+
+    def __unicode__(self):
+        """Return unicode representaion of name."""
+        return u"{}".format(self.name)
+
+
+class Color(models.Model):
+    """The colors available in mtg."""
+
+    COLOR_CHOICES = (
+        ('white', 'W'),
+        ('blue', 'U'),
+        ('black', 'B'),
+        ('green', 'G'),
+        ('red', 'R'),
+        ('colorless', 'C'))
+
+    name = models.CharField(max_length=1,
+                            choices=COLOR_CHOICES,
+                            unique=True)
+
+    def __unicode__(self):
+        """Return unicode representation of name."""
+        return u"{}".format(self.name)
+
+
+class Card(models.Model):
+    """Class for each individual card."""
+
+    name = models.CharField(max_length=40)
+    manacost = models.CharField(max_length=40)
+    #converted_manacost = models.PositiveIntegerField()
+
+    # Foreign Keys
+    cardtype = models.ForeignKey(CardType,
+                                 on_delete=models.PROTECT)
+
+    # ManyToMany Fields:
+    colors = models.ManyToManyField(Color)
+
+    def __unicode__(self):
+        return u"{}".format(self.name)
